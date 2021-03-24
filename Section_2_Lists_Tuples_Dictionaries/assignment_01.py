dc81b86b738313c44bde48f5ddfbac42623d0b72
berkgonencc <berkgonencc@gmail.com>
2021-03-24 17:35:33 +0300

    new commit

---
+++ Section_2_Lists_Tuples_Dictionaries/assignment_01.py
@@ -0,0 +1,13 @@
+# Assignment 1:
+"""
+Print Bill's salary from the my_list object shown below.
+
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
+
+"""
+
+
+# your code below:
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
+salary_of_bill = my_list[0].get('Bill')
+print(salary_of_bill)
---
+++ Section_2_Lists_Tuples_Dictionaries/assignment_02.py
@@ -0,0 +1,16 @@
+# Assignment 2:
+"""
+Using some of the collection data types we learned about
+in the course so such as a list and dictionary, create a
+data structure that best represents the following scenario:
+
+Tom has a salary of 20000 and is 22 years old. He owns a few items such as
+a jacket, a car, and TV. Mike is another person who makes 24000 and is 27 years old
+who owns a bike, a laptop and boat.
+"""
+
+# your code below:
+new_list = [{'Tom':{'Salary':20000, 'Age':22, 'Items':['jacket','car','TV']},
+        'Mike':{'Salary':24000, 'Age':27, 'Items':['bike','laptop','boat']}}]
+
+print(new_list)
